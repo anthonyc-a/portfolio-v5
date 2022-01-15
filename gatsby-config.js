deb60852c1fbac4068c6eae766025917b4d7fc1c
anthonyc-a <anthonyc.animba@gmail.com>
2022-01-15 15:35:30 +0000

    -

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/img`,
+        path: `${__dirname}/static/img`,
       },
     },
     {
