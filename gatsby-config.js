c068e4e1630e581e65d5b2aa4e772d4cdf434dfe
anthonyc-a <anthonyc.animba@gmail.com>
2022-01-16 10:17:39 +0000

    plugin manifest update

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,24 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
+        name: `static`,
+        path: `${__dirname}/static`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `anthony-animba-portfolio`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#131313`,
+        display: `minimal-ui`,
+        icon: `static/logo512.png`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
         name: `images`,
         path: `${__dirname}/static/img`,
       },
