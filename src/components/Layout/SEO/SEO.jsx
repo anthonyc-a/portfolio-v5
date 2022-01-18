c6526c65141b7b7b2e3833dad9cbef04b0d94191
anthonyc-a <anthonyc.animba@gmail.com>
2022-01-18 08:11:22 +0000

    SEO changes

--- src/components/Layout/SEO/SEO.jsx
+++ src/components/Layout/SEO/SEO.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { Helmet } from "react-helmet"
 import { ThemeStateContext } from "../../context/ToggleThemeProveide"
+import icon from "../../../../static/logo192.png"
 
 const Seo = () => {
   const theme = React.useContext(ThemeStateContext)
@@ -8,12 +9,18 @@ const Seo = () => {
   return (
     <Helmet>
       <html lang="en" className={themeClass} />
-      <title>Anthony Animba | Freelance Web Designer & Developer in London</title>
+      <title>
+        Anthony Animba | Freelance Web Designer & Developer in London
+      </title>
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no"
       />
-      <meta name="description" content="A freelance web designer and web developer based in London" />
+      <meta
+        name="description"
+        content="Anthony Animba, a freelance web designer and developer based in London. I work with individuals and small to medium sized businesses to help them stand out on the web."
+      />
+      <link rel="icon" href={icon} type="image/png" />
     </Helmet>
   )
 }
